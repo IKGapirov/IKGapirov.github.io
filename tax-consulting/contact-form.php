ba1e5277a7bca2515763b8529ba0e1114126c7b2
Ikrom Gapirov <IKGapirov@gmail.com>
2021-09-10 16:01:55 +0500

    fix

--- tax-consulting/contact-form.php
+++ tax-consulting/contact-form.php
@@ -3,10 +3,10 @@
 <div id="form_163442_1"></div>
 <script type="text/javascript">
    (function (d, w, c) {
-       (w[c] = w[c] || []).push({formId:163442,host:"formdesigner.ru",formHeight:100, el: "form_163442_1",center: 1,scroll: 0});
+       (w[c] = w[c] || []).push({formId:163442,host:"ikgapirov.github.io/tax-consulting",formHeight:100, el: "form_163442_1",center: 1,scroll: 0});
        var s = d.createElement("script"), g = "getElementsByTagName";
        s.type = "text/javascript"; s.charset="UTF-8"; s.async = true;
-       s.src = (d.location.protocol == "https:" ? "https:" : "http:")+"//formdesigner.ru/js/iform.js?v=0.0.2";
+       s.src = (d.location.protocol == "https:" ? "https:" : "http:")+"//ikgapirov.github.io/tax-consulting/js/iform.js?v=0.0.2";
        var h=d[g]("head")[0] || d[g]("body")[0];
        h.appendChild(s);
    })(document, window, "fdforms");
