c0af00f7d8223104932d65f8f403bb07d1afec2b
Ikrom Gapirov <IKGapirov@gmail.com>
2021-09-10 11:46:53 +0500

    Big change

--- tax-consulting/mail.php
+++ tax-consulting/mail.php
@@ -1,10 +1,11 @@
  <?php
 $to = "IKGapirov@gmail.com"; // емайл получателя данных из формы
-$tema = "Форма обратной связи на PHP"; // тема полученного емайла
-$message = "Ваше имя: ".$_POST['name']."<br>";//присвоить переменной значение, полученное из формы name=name
-  $message .= "E-mail: ".$_POST['email']."<br>"; //полученное из формы name=email
-$message .= "Номер телефона: ".$_POST['phone']."<br>"; //полученное из формы name=phone
-$message .= "Сообщение: ".$_POST['message']."<br>"; //полученное из формы name=message
+$tema = "Send Us A Message"; // тема полученного емайла
+$message = "Tell us your name * ".$_POST['first-name']."<br>";//присвоить переменной значение, полученное из формы name=name
+$message = "Tell us your name * ".$_POST['last-name']."<br>";//присвоить переменной значение, полученное из формы name=name
+  $message .= "Enter your email * ".$_POST['email']."<br>"; //полученное из формы name=email
+$message .= "Enter phone number ".$_POST['phone']."<br>"; //полученное из формы name=phone
+$message .= "Message * ".$_POST['message']."<br>"; //полученное из формы name=message
 $headers  = 'MIME-Version: 1.0' . "\r\n"; // заголовок соответствует формату плюс символ перевода строки
   $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n"; // указывает на тип посылаемого контента
 mail($to, $tema, $message, $headers); //отправляет получателю на емайл значения переменных
