b1b286d3c6cb23a5ce916d8b49b4935ebdfc2e6e
Ikrom Gapirov <IKGapirov@gmail.com>
2021-09-10 15:03:00 +0500

    Update mail.php

--- tax-consulting/php/mail.php
+++ tax-consulting/php/mail.php
@@ -1,22 +1,50 @@
 <?php
-if (isset($_POST['first-name'])) {$name = $_POST['first-name']; if ($name == '') {unset($name);}}
-if (isset($_POST['last-name'])) {$name = $_POST['last-name']; if ($name == '') {unset($name);}}
-if (isset($_POST['mail'])) {$mail = $_POST['mail']; if ($mail == '') {unset($mail);}}
-if (isset($_POST['phone'])) {$phone = $_POST['phone']; if ($phone == '') {unset($phone);}}
-if (isset($_POST['message'])) {$message = $_POST['message']; if ($message == '') {unset($message);}}
- 
-if (isset($name) && isset($mail) && isset($phone) && isset($message)){
- 
-$address = "IKGapirov@gmail.com";
-$mes = "Имя: $name \nE-mail: email \nТема: $phone \nТекст: $message";
-$send = mail ($address,$sub,$mes,"Content-type:text/plain; charset = UTF-8\r\nFrom:$mail");
-if ($send == 'true')
-{echo "Сообщение отправлено успешно. Перейдите на главную страницу блога <a href='https://bloggood.ru/'>BLOGGOOD.RU</a>,и вы сможете продолжить ваш просмотр";}
-else {echo "Ошибка, сообщение не отправлено!";}
- 
+
+// Replace this with your own email address
+$to = 'IKGapirov@gmail.com';
+
+function url(){
+  return sprintf(
+    "%s://%s",
+    isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
+    $_SERVER['SERVER_NAME']
+  );
 }
-else
-{
-echo "Заполнили все поля!!!";
+
+if($_POST) {
+
+   $name = trim(stripslashes($_POST['last-name']));
+   $email = trim(stripslashes($_POST['mail']));
+   $phone = trim(stripslashes($_POST['phone']));
+   $contact_message = trim(stripslashes($_POST['message']));
+
+
+   
+	if ($subject == '') { $subject = "Contact Form Submission"; }
+
+   // Set Message
+   $message .= "Email from: " . $name . "<br />";
+	 $message .= "Email address: " . $email . "<br />";
+   $message .= "Phone: " . $phone . "<br />";
+   $message .= "Message: <br />";
+   $message .= nl2br($contact_message);
+   $message .= "<br /> ----- <br /> This email was sent from your site " . url() . " contact form. <br />";
+
+   // Set From: header
+   $from =  $name . " <" . $email . ">";
+
+   // Email Headers
+	$headers = "From: " . $from . "\r\n";
+	$headers .= "Reply-To: ". $email . "\r\n";
+ 	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+   ini_set("sendmail_from", $to); // for windows server
+   $mail = mail($to, $subject, $message, $headers);
+
+	if ($mail) { echo "OK"; }
+   else { echo "Something went wrong. Please try again."; }
+
 }
+
 ?>
