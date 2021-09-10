bf20e490c045c1e9c29178221dee802b6de9c2b4
Ikrom Gapirov <IKGapirov@gmail.com>
2021-09-10 16:37:57 +0500

    Update contact-form.php

--- tax-consulting/contact-form.php
+++ tax-consulting/contact-form.php
@@ -1,13 +1,1 @@
-﻿<?php
-
-$to = 'IKGapirov@gmail.com'; // адрес получателя
-$message = "Имя: {$_POST['name']}\r\n"; // добавляем имя в текст
-$headers .= 'Content-type: text/plain; charset=utf-8' . "\r\n"; // установливаем кодировку
-$headers .= 'From: mail@site.com' . "\r\n"; // добавляем отправителя
- 
-if( mail($to, $subject, $message, $headers) ){
- echo '<p style="color: green;">Ваше сообщение отправлено</p>';
-}else{
- echo '<p style="color: red;">Ошибка!</p>';
-}
-?>
+﻿<?php header("Content-Type: text/html; charset=utf-8"); if(isset($_SERVER["HTTP_X_REQUESTED_WITH"]) && strtolower($_SERVER["HTTP_X_REQUESTED_WITH"]) === "xmlhttprequest") { if(!isset($_POST["name"]) || !isset($_POST["email"]) || !isset($_POST["message"])) { die(); } function send_form($message) { $mail_to = "IKGapirov@gmail.com"; // Адрес, куда отправляем письма $subject = "Письмо с обратной связи"; // Тема письма $headers = "MIME-Version: 1.0\r\n"; $headers .= "Content-type: text/html; charset=utf-8\r\n"; $headers .= "From: Система уведомлений <no-reply@".$_SERVER['HTTP_HOST'].">\r\n"; mail($mail_to, $subject, $message, $headers); } $name = strip_tags($_POST["name"]); // Имя $email = strip_tags($_POST["email"]); // E-mail $mess = strip_tags($_POST["message"]); // Сообщение if(!preg_match("|^([a-z0-9_.-]{1,20})@([a-z0-9.-]{1,20}).([a-z]{2,4})|is", strtolower($email))) { // Если e-mail пустой или невалиден echo "E-mail указан некорректно."; die(); } if($mess == "") { // Если сообщение пустое echo "Не указан текст сообщения."; die(); } if($name == "") { // Если имя не указано $name = "Не указано"; } $message = <<<HTML <b>Имя отправителя</b>: {$name}<br> <b>E-mail</b>: {$email}<br><br> <b>Текст письма</b>: {$mess} HTML; send_form($message); // Если ранее описанных ошибок нет - отправляем сообщение echo "Сообщение успешно отправлено!"; } else { die(); } ?>
