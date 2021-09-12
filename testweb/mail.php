eac6035dbacbc550ab65e55af8eda05b17054fe4
Ikrom Gapirov <IKGapirov@gmail.com>
2021-09-12 11:31:26 +0500

    fix

---
+++ testweb/mail.php
@@ -0,0 +1,16 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $to = 'IKGapirov@gmail.com';
+    $subject = 'Customer Inquiry';
+    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+
+    if ($_POST['submit']) {
+        if (mail ($to, $subject, $body, $from)) {
+            echo '<p>Your message has been sent!</p>';
+        } else {
+            echo '<p>Something went wrong, go back and try again!</p>';
+        }
+    }
+?>
